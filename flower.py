6ad097e69081b1d425e7c46c8e6efc838dcb94f0
skudunter <danielgruttercpt@gmail.com>
2024-09-26 09:34:58 +0200

    feat added pollen to flowers

--- flower.py
+++ flower.py
@@ -1,4 +1,6 @@
-from typing import Any
+from typing import Any, List
+
+
 class Pollen:
     def __init__(self, information: Any):
         self.information = information
@@ -8,14 +10,12 @@ class Flower:
     def __init__(self, row: int, col: int, pollen_type: str):
         self.icon = 'F'
         self.pollen_type = pollen_type
-        self.children = []
+        self.pollens: List[Pollen] = []
         self.row = row
         self.col = col
 
     def add_pollen(self, pollen: Pollen):
-        self.children.append(pollen)
-    
+        self.pollens.append(pollen)
+
     def get_position(self):
         return self.row, self.col
-
-
